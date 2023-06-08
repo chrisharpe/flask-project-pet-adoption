8b8b39e02bb5d642b1e092c253e3c9cc31698ec6
chrisharpe <4chrisharpe@gmail.com>
2023-06-08 16:28:57 -0400

    collects form submissions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import (Flask, render_template,
+                   url_for, request)
 
 
 app = Flask(__name__)
@@ -9,8 +10,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/add-pet')
+@app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
+    print(request.form)
+    print(request.form['name'])
     return render_template('addpet.html')
 
 
