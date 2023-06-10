75237951956f9757202ac8cc60de5dc0e563b5e7
chrisharpe <4chrisharpe@gmail.com>
2023-06-09 21:33:41 -0400

    links database to form fill

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import (render_template,
-                   url_for, request)
+                   url_for, request, redirect)
 
 from models import db, Pet, app
 
@@ -11,8 +11,12 @@ def index():
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
-    print(request.form)
-    print(request.form['name'])
+    if request.form:
+        new_pet = Pet(name=request.form['name'], age=request.form['age'],
+                      breed=request.form['breed'], color=request.form['color'], size=request.form['size'], weight=request.form['weight'], url=request.form['url'], url_tag=request.form['alt'], pet_type=request.form['pet'], gender=request.form['gender'], spay=request.form['spay'], house_trained=request.form['housetrained'], description=request.form['description'])
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('addpet.html')
 
 
